4fd9295abff92203eb6eb966143720f0b20469ee
NineNights195 <ninenights19534@gmail.com>
2025-09-30 14:05:01 +0700

    Update yolo-test.py: open both cameras and detect only vehicles and person and counting them

--- backend/scripts/yolo-test.py
+++ backend/scripts/yolo-test.py
@@ -3,28 +3,45 @@ import cv2
 from ultralytics import YOLO
 import time
 
-# Initialize camera
-picam2 = Picamera2()
-video_config = picam2.create_video_configuration(main={"format": "RGB888"})
-picam2.configure(video_config)
-picam2.start()
+# Initialize two cameras
+picam2_0 = Picamera2(0)
+picam2_1 = Picamera2(1)
+
+video_config_0 = picam2_0.create_video_configuration(main={"format": "RGB888"})
+video_config_1 = picam2_1.create_video_configuration(main={"format": "RGB888"})
+
+picam2_0.configure(video_config_0)
+picam2_1.configure(video_config_1)
+
+picam2_0.start()
+picam2_1.start()
 time.sleep(2)  # camera warm-up
 
 # Load YOLOv8 model (nano for speed)
 model = YOLO("yolov8n.pt")
 
-print("Starting YOLO detection. Press 'q' to quit.")
+# COCO class IDs
+# Vehicles: 0 = person, 1 = bicycle, 2 = car, 3 = motorcycle, 5 = bus, 7 = truck
+target_classes = [0,1,2,3,5,7]
+
+print("Starting YOLO detection on two cameras. Press 'q' to quit.")
 try:
     while True:
-        frame = picam2.capture_array("main")
+        frame0 = picam2_0.capture_array("main")
+        frame1 = picam2_1.capture_array("main")
         
         # YOLO inference
-        results = model(frame, verbose=False)[0]  # returns list of detections
-        annotated_frame = results.plot() # Draw boxes
-        cv2.imshow("YOLO Test", annotated_frame)
+        results0 = model(frame0, classes=target_classes)[0]
+        results1 = model(frame1, classes=target_classes)[0]
+        annotated_frame0 = results0.plot()
+        annotated_frame1 = results1.plot()
+        
+        cv2.imshow("YOLO Test Camera 0", annotated_frame0)
+        cv2.imshow("YOLO Test Camera 1", annotated_frame1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 finally:
-    picam2.stop()
+    picam2_0.stop()
+    picam2_1.stop()
     cv2.destroyAllWindows()
